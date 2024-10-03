f20fd1b677b15af879fed7750a87b451b0d59343
Irene Toxla <toxlairene@gmail.com>
2024-10-03 15:46:29 +0000

    added changes

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ import express from 'express';
 import { bookingRouter } from './routes/route.js';
 import mongoose from 'mongoose';
 import "dotenv/config";
-
-await mongoose.connect(process.env.MONGO_URI);
-
+import cors from 'cors'
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
+
 //define route
 app.use(bookingRouter);
 
+await mongoose.connect(process.env.MONGO_URI);
 
 //listen for incoming request
 app.listen(3001,() => {
