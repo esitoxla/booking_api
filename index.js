0bdfed36d879d80dc324a6c3e77202b951d6c763
Irene Toxla <toxlairene@gmail.com>
2025-01-03 23:34:04 +0000

    added changes in index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json())
 //define route
 app.use(bookingRouter);
 
-await mongoose.connect(process.env.MONGO_URl);
+await mongoose.connect(process.env.MONGO_URI);
 
 //listen for incoming request
 app.listen(3001,() => {
